5d81f72d61e0d8e2246599292d8a8df0447c99eb
Neo <meetneo@outlook.com>
2015-12-06 09:31:08 +0800

    Configure debug

--- road_map.py
+++ road_map.py
@@ -4,7 +4,7 @@ __author__ = 'Neo'
 import os
 import pickle
 import datetime
-from utilities import write_json
+from utilities import write_json, read_json
 from collections import defaultdict
 
 DATA_DIR = "Raw"
@@ -56,6 +56,8 @@ def get_whole_ways(tags_list, whole_nodes):
     ret_dict = dict()
     j = 0
     s_time = datetime.datetime.now()
+    with open('whole_ways.log', 'a') as log_file:
+        log_file.flush()
     for item in tags_list:
         ele_type = item[unicode("type")]
         if ele_type == "way":
@@ -162,24 +164,74 @@ def get_ways_slices_modify(whole_ways, intersect_points):
 
 if __name__ == "__main__":
     raw_data = get_raw_data(SAMPLE_DATA)
-    whole_nodes = get_whole_nodes(raw_data = raw_data)
+    # whole_nodes = get_whole_nodes(raw_data = raw_data)
+
+    s_time = datetime.datetime.now()
+    whole_nodes = read_json("whole_nodes.txt")
+    with open('test.log', 'a') as log_file:
+        e_time = datetime.datetime.now()
+        cost_time = e_time - s_time
+        log = 'Reading nodes finished at ' + str(cost_time) + '\n'
+        log_file.write(log)
+
     # print "whole nodes:", len(whole_nodes.keys())
     # print "after 1"
     # write_json("whole_nodes.txt", whole_nodes)
     # print "write 1"
-    tags_list = get_tags_list(raw_data = raw_data)
+
+    # tags_list = get_tags_list(raw_data = raw_data)
     print "after 2"
-    whole_ways = get_whole_ways(tags_list=tags_list, whole_nodes=whole_nodes)
+
+    whole_ways = read_json("whole_ways.txt")
+
+    # whole_ways = get_whole_ways(tags_list=tags_list, whole_nodes=whole_nodes)
     print "after 3"
-    write_json("whole_ways.txt", whole_ways)
+    # write_json("whole_ways.txt", whole_ways)
     print "write 3"
-    # nodes_freq = get_node_count(whole_ways)
-    # print "after 4"
-    # new_inter_points = get_intersection_point_modify(nodes_freq, whole_nodes=whole_nodes)
-    # print "after 5"
-    # write_json("new_whole_inter.txt", new_inter_points)
-    # print "write 5"
-    # new_whole_slices = get_ways_slices_modify(whole_ways, intersect_points=new_inter_points)
-    # print "after 6"
-    # write_json("modify_whole_slices.txt", new_whole_slices)
-    # print "write 6"
+
+    s_time = datetime.datetime.now()
+    nodes_freq = get_node_count(whole_ways)
+    with open('test.log', 'a') as log_file:
+        e_time = datetime.datetime.now()
+        cost_time = e_time - s_time
+        log = 'Counting nodes finished at ' + str(cost_time) + '\n'
+        log_file.write(log)
+
+    print "after 4"
+
+    s_time = datetime.datetime.now()
+    new_inter_points = get_intersection_point_modify(nodes_freq, whole_nodes=whole_nodes)
+    with open('test.log', 'a') as log_file:
+        e_time = datetime.datetime.now()
+        cost_time = e_time - s_time
+        log = 'Finding inter points finished at ' + str(cost_time) + '\n'
+        log_file.write(log)
+
+    print "after 5"
+
+    s_time = datetime.datetime.now()
+    write_json("new_whole_inter.txt", new_inter_points)
+    with open('test.log', 'a') as log_file:
+        e_time = datetime.datetime.now()
+        cost_time = e_time - s_time
+        log = 'Writing inter nodes finished at ' + str(cost_time) + '\n'
+        log_file.write(log)
+    print "write 5"
+
+    s_time = datetime.datetime.now()
+    new_whole_slices = get_ways_slices_modify(whole_ways, intersect_points=new_inter_points)
+    with open('test.log', 'a') as log_file:
+        e_time = datetime.datetime.now()
+        cost_time = e_time - s_time
+        log = 'Finding way slices finished at ' + str(cost_time) + '\n'
+        log_file.write(log)
+    print "after 6"
+
+    s_time = datetime.datetime.now()
+    write_json("modify_whole_slices.txt", new_whole_slices)
+    with open('test.log', 'a') as log_file:
+        e_time = datetime.datetime.now()
+        cost_time = e_time - s_time
+        log = 'Writing way slices finished at ' + str(cost_time) + '\n'
+        log_file.write(log)
+    print "write 6"
